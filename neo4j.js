324a0b8d0e9cd565bf4a7471bc678e48eb6fe2ad
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-09-16 00:36:24 +0300

    connect to neo4j db

---
+++ neo4j.js
@@ -0,0 +1,31 @@
+const neo4j = require('neo4j-driver');
+
+let driver
+
+
+const initDriver = (uri, username, password) => {
+  driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
+
+  return driver.verifyConnectivity()
+    .then(() => {
+        console.log('Connected to Neo4j')
+        return driver
+    }).catch((err) => {
+        console.log('Error connecting to Neo4j', err)
+        throw err
+    })
+}
+
+const getDriver = () => {
+  return driver
+}
+
+const closeDriver = () => {
+  return driver && driver.close()
+}
+
+module.exports = {
+    initDriver,
+    getDriver,
+    closeDriver,
+}
--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const { initDriver } = require('./neo4j');
 
 dotenv.config();
 
 app.use(bodyParser.json());
 
+// Connect to Neo4j and Verify Connectivity
+const {
+    NEO4J_URI,
+    NEO4J_USERNAME,
+    NEO4J_PASSWORD,
+} = process.env
+initDriver(NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD)
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
