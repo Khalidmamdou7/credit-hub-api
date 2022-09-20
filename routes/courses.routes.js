e2783f68aba5474b0d0b41a228e57858fac183d5
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-09-20 19:22:16 +0300

    update courses router to use courses.services

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,23 +1,49 @@
 const coursesRouter = require('express').Router();
+const coursesService = require('../services/courses.services');
 
-coursesRouter.get('/', (req, res) => {
-    res.send('Getting all courses');
+coursesRouter.get('/', async (req, res) => {
+    try {
+        const courses = await coursesService.getAllCourses();
+        res.json(courses);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
-coursesRouter.get('/:code', (req, res) => {
-    res.send('Gettting course with code ' + req.params.code);
+coursesRouter.get('/:code', async (req, res) => {
+    try {
+        const course = await coursesService.getCourse(req.params.code);
+        res.json(course);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
-coursesRouter.post('/', (req, res) => {
-    res.send('Adding a course');
+coursesRouter.post('/', async (req, res) => {
+    try {
+        const course = await coursesService.createCourse(req.body.code, req.body.name);
+        res.json(course);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
-coursesRouter.put('/:code', (req, res) => {
-    res.send('Updating course with code ' + req.params.code);
+coursesRouter.put('/:code', async (req, res) => {
+    try {
+        const course = await coursesService.updateCourse(req.params.code, req.body.name);
+        res.json(course);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
-coursesRouter.delete('/:code', (req, res) => {
-    res.send('Deleting course with code ' + req.params.code);
+coursesRouter.delete('/:code', async (req, res) => {
+    try {
+        await coursesService.deleteCourse(req.params.code);
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 module.exports = coursesRouter;
