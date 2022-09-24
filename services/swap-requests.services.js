8d0fcce4a5c530bf561a91ed485e0a004271d324
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-09-24 14:24:56 +0300

    feat: create swap-requests controller

---
+++ services/swap-requests.services.js
@@ -0,0 +1,21 @@
+const { getDriver } = require('../neo4j/neo4j');
+
+const getSwapRequests = async (user) => {
+}
+
+const createSwapRequest = async (user, timeslot) => {
+}
+
+const updateSwapRequest = async (user, timeslotId, timeslot) => {
+}
+
+const deleteSwapRequest = async (user, timeslotId) => {
+}
+
+
+module.exports = {
+    getSwapRequests,
+    createSwapRequest,
+    updateSwapRequest,
+    deleteSwapRequest
+};
