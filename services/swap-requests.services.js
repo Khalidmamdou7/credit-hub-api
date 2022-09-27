84b3ee03781dcdb7a2c73eb482d33d5095db7273
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-09-28 00:40:23 +0300

    feat: implement checkMatch and agreetoSwap Service

--- services/swap-requests.services.js
+++ services/swap-requests.services.js
@@ -49,7 +49,53 @@ const createSwapRequest = async (user, timeslot) => {
             throw new NotFoundError('Offered timeslot not found');
         }
         const sr = res3.records[0].get('sr').properties;
-        return sr;
+        const ot = res3.records[0].get('ot').properties;
+        const wt = res3.records[0].get('wt').properties;
+        // check if the swap request matches any other swap requests
+        const matches = await checkSwapRequestMatches(user, sr.id);
+        return {
+            ...sr,
+            offeredTimeslot: ot,
+            wantedTimeslots: [wt],
+            matches
+        };
+    }
+    finally {
+        await session.close();
+    }
+}
+
+const checkSwapRequestMatches = async (user, swapRequestId) => {
+    const driver = getDriver();
+    const session = driver.session();
+    try {
+        const res = await session.writeTransaction(tx =>
+            tx.run(
+                `MATCH (u:User {userId: $userId})-[:REQUESTED]->(sr:SwapRequest {swapRequestId: $swapRequestId})
+                MATCH (sr)-[:OFFERS]->(ot:Timeslot)
+                MATCH (sr)-[:WANTS]->(wt:Timeslot)
+                MATCH (sr2:SwapRequest)-[:OFFERS]->(wt)
+                MATCH (sr2)-[:WANTS]->(ot)
+                MATCH (sr2)<-[:REQUESTED]-(u2:User)
+                WHERE sr2.status = 'pending'
+                AND NOT (u)-[:REQUESTED]->(sr2)
+                SET sr.status = 'waiting-for-agreement'
+                SET sr2.status = 'waiting-for-agreement'
+                CREATE (sr)-[:MATCHES {status: 'waiting-for-agreement'}]->(sr2)
+                RETURN sr, sr2, u2`,
+                { userId: user.userId, swapRequestId }
+            )
+        );
+        return res.records.map(record => {
+            const sr = record.get('sr').properties;
+            const sr2 = record.get('sr2').properties;
+            const u2 = record.get('u2').properties;
+            return {
+                ...sr,
+                matchedSwapRequest: sr2,
+                matchedUser: u2.email
+            };
+        });
     }
     finally {
         await session.close();
@@ -60,12 +106,15 @@ const getSwapRequests = async (user) => {
     const driver = getDriver();
     const session = driver.session();
     try {
+        // if a swap request status is waiting-for-agreement, return the matching users too
         const res = await session.readTransaction(tx =>
             tx.run(
                 `MATCH (u:User {userId: $userId})-[:REQUESTED]->(sr:SwapRequest)
                 MATCH (sr)-[:OFFERS]->(ot:Timeslot)
                 MATCH (sr)-[:WANTS]->(wt:Timeslot)
-                RETURN sr, ot, wt`,
+                OPTIONAL MATCH (sr)-[:MATCHES]->(sr2:SwapRequest)
+                OPTIONAL MATCH (sr2)<-[:REQUESTED]-(u2:User)
+                RETURN sr, ot, wt, sr2, u2`,
                 { userId: user.userId }
         ));
         // for each swap request, get the offered and wanted timeslots. if there are duplicates swap requests,
@@ -75,11 +124,15 @@ const getSwapRequests = async (user) => {
             const sr = record.get('sr').properties;
             const ot = record.get('ot').properties;
             const wt = record.get('wt').properties;
+            const sr2 = record.get('sr2') ? record.get('sr2').properties : null;
+            const u2 = record.get('u2') ? record.get('u2').properties : null;
             if (!swapRequests[sr.id]) {
                 swapRequests[sr.id] = {
                     ...sr,
                     offeredTimeslot: ot,
-                    wantedTimeslots: [wt]
+                    wantedTimeslots: [wt],
+                    matchedSwapRequest: sr2,
+                    matchedUser: u2 ? u2.email : null
                 };
             }
             else {
@@ -113,7 +166,67 @@ const updateSwapRequest = async (user, timeslotId, timeslot) => {
                 { userId: user.userId, swapRequestId: timeslotId, wantedTimeslots, offeredTimeslot }
             )
         );
-        return res.records[0].get('sr').properties;
+        const sr = res.records[0].get('sr').properties;
+        const w = res.records[0].get('w').properties;
+        const o = res.records[0].get('o').properties;
+        // check if the swap request matches any other swap requests
+        const matches = await checkSwapRequestMatches(user, sr.id);
+        return {
+            ...sr,
+            offeredTimeslot: o,
+            wantedTimeslots: [w],
+            matches
+        };
+    }
+    finally {
+        await session.close();
+    }
+}
+
+const agreeSwapRequest = async (user, swapRequestId, matchedSwapRequestId) => {
+    const driver = getDriver();
+    const session = driver.session();
+    try {
+        // Regarding the matched swap request SET the matches relationship status to 'agreed' only if both swap requests are in 'agreed' status
+        // if there is a multiple matched swap requests, then the other matched swap requests will be deleted and set to 'pending'.
+        const res = await session.writeTransaction(tx =>
+            tx.run(
+                `MATCH (u:User {userId: $userId})-[:REQUESTED]->(sr:SwapRequest {swapRequestId: $swapRequestId})
+                MATCH (sr)-[m1:MATCHES]-(sr2:SwapRequest {swapRequestId: $matchedSwapRequestId})
+                MATCH (sr)-[:OFFERS]->(ot:Timeslot)
+                MATCH (sr)-[:WANTS]->(wt:Timeslot)
+                MATCH (sr2)-[:OFFERS]->(ot2:Timeslot)
+                MATCH (sr2)-[:WANTS]->(wt2:Timeslot)
+                SET sr.status = 'agreed'
+                SET m1.status = CASE WHEN sr2.status = 'agreed' THEN 'agreed' ELSE m1.status END
+                MATCH (sr)-[m2:MATCHES]->(sr3:SwapRequest)
+                WHERE sr3 <> sr2
+                DELETE m2
+                MATCH (sr3)-[m3:MATCHES]->(sr4:SwapRequest)
+                WHERE sr4 <> sr
+                SET sr4.status = CASE WHEN m3.status IS NULL THEN 'pending' ELSE sr3.status END
+                RETURN sr, ot, wt, sr2, ot2, wt2, m1`,
+                { userId: user.userId, swapRequestId, matchedSwapRequestId }
+            )
+        );
+        const sr = res.records[0].get('sr').properties;
+        const ot = res.records[0].get('ot').properties;
+        const wt = res.records[0].get('wt').properties;
+        const sr2 = res.records[0].get('sr2').properties;
+        const ot2 = res.records[0].get('ot2').properties;
+        const wt2 = res.records[0].get('wt2').properties;
+        const m1 = res.records[0].get('m1').properties;
+        return {
+            ...sr,
+            offeredTimeslot: ot,
+            wantedTimeslots: [wt],
+            matchedSwapRequest: {
+                ...sr2,
+                offeredTimeslot: ot2,
+                wantedTimeslots: [wt2],
+                matches: m1
+            }
+        };
     }
     finally {
         await session.close();
@@ -124,17 +237,18 @@ const deleteSwapRequest = async (user, swapRequestId) => {
     const driver = getDriver();
     const session = driver.session();
     try {
+        // if a swap request status is waiting-for-agreement, set the status of the matching swap request to pending
         const res = await session.writeTransaction(tx =>
             tx.run(
                 `MATCH (u:User {userId: $userId})-[:REQUESTED]->(sr:SwapRequest {swapRequestId: $swapRequestId})
-                DETACH DELETE sr RETURN sr`,
+                MATCH (sr)-[m:MATCHES]->(sr2:SwapRequest)
+                OPTIONAL MATCH (sr2)-[m2:MATCHES]->(sr3:SwapRequest)
+                SET sr2.status = CASE WHEN m2 IS NULL THEN 'pending' ELSE sr2.status END
+                DETACH DELETE sr`,
                 { userId: user.userId, swapRequestId }
             )
         );
-        if (res.records.length === 0) {
-            throw new NotFoundError('Swap request not found');
-        }
-        return res.records[0].get('sr').properties;
+        return res.records.length > 0;
     }
     finally {
         await session.close();
@@ -146,5 +260,6 @@ module.exports = {
     getSwapRequests,
     createSwapRequest,
     updateSwapRequest,
-    deleteSwapRequest
+    deleteSwapRequest,
+    agreeSwapRequest
 };
