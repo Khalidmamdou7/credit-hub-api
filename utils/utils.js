0ad6dff088264de94b07f9073fd2d4b045496f32
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-09-29 01:24:39 +0300

    bug-fix: fix url in confirmation mail typo

--- utils/utils.js
+++ utils/utils.js
@@ -40,12 +40,12 @@ const sendConfirmationEmail = async (email, userId, token) => {
             <div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; width: 500px; margin: 0 auto;">
                 <h1 style="text-align: center; color: #4d4d4d;">Confirm your email</h1>
                 <p style="text-align: center; color: #4d4d4d;">Please click the button below to confirm your email.</p>
-                <a href="http://localhost:3000/confirm/${userId}/${token}" style="text-decoration: none; display: block; margin: 0 auto; width: 200px; background-color: #4d4d4d; color: #f2f2f2; padding: 10px; border-radius: 10px; text-align: center;">Confirm</a>
+                <a href="http://${process.env.DOMAIN}/api/auth/confirm/${userId}/${token}" style="text-decoration: none; display: block; margin: 0 auto; width: 200px; background-color: #4d4d4d; color: #f2f2f2; padding: 10px; border-radius: 10px; text-align: center;">Confirm</a>
                 <p style="text-align: center; color: #4d4d4d;">If you can't click on the button, please copy the following link to your browser</p>
                 <p style="text-align: center; color: #4d4d4d;">http://${process.env.DOMAIN}/api/auth/confirm/${userId}/${token}</p>
                 <br>
                 <p style="text-align: center; color: #4d4d4d;">Thank you for using our website</p>
-                <p style="text-align: center; color: #4d4d4d;">The <a href="http://${process.env.DOMAIN}" style="text-decoration: none; color: #4d4d4d;">Swap Courses</a> team</p>
+                <p style="text-align: center; color: #4d4d4d;">The <a href="http://${process.env.CLIENT_URL}" style="text-decoration: none; color: #4d4d4d;">Swap Courses</a> team</p>
                 <hr style="border: 0.5px solid #4d4d4d; width: 100%; margin: 20px 0;">
                 <p style="text-align: center; color: #4d4d4d;">If you didn't sign up for an account, you can safely delete this email</p>
             </div>
